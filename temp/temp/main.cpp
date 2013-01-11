32d0e78b2493afb75fa2217d13638e56b6beb195
Kaushal <kpatel1515@gmail.com>
2013-01-11 14:00:51 -0600

    intro

---
+++ temp/temp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  temp
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
